9be892e372dd695020ebf280e965e1feb857d044
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-05 13:41:58 +0100

    Data types

--- rest_api_tests.go
+++ rest_api_tests.go
@@ -1,7 +1,47 @@
+/*
+Copyright © 2022 Pavel Tisnovsky
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import "fmt"
 
+// StatusOnlyResponse represents response containing just a status
+type StatusOnlyResponse struct {
+	Status string `json:"status"`
+}
+
+// ClustersResponse represents response containing list of clusters for given
+// organization
+type ClustersResponse struct {
+	Clusters []string `json:"clusters"`
+	Status   string   `json:"status"`
+}
+
+// RestAPITest represents specification of one REST API call (request) and
+// expected response
+type RestAPITest struct {
+	Endpoint       string
+	Method         string
+	Message        string
+	AuthHeader     bool
+	ExpectedStatus int
+	ExpectedType   string
+}
+
 func main() {
 	fmt.Println("vim-go")
+
 }
