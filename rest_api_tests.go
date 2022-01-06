8714ace91ad0f89a7c98158b0c9b3f5e0cd2c7d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-06 10:40:58 +0100

    Utility functions

--- rest_api_tests.go
+++ rest_api_tests.go
@@ -18,6 +18,58 @@ package main
 
 import "fmt"
 
+// setAuthHeaderForOrganization set authorization header to request
+func setAuthHeaderForOrganization(f *frisby.Frisby, orgID int) {
+	plainHeader := fmt.Sprintf("{\"identity\": {\"internal\": {\"org_id\": \"%d\"}, \"account_number\":\"%s\"}}", orgID, accountNumber)
+	encodedHeader := base64.StdEncoding.EncodeToString([]byte(plainHeader))
+	f.SetHeader(authHeaderName, encodedHeader)
+}
+
+// setAuthHeader set authorization header to request for organization 1
+func setAuthHeader(f *frisby.Frisby) {
+	setAuthHeaderForOrganization(f, 1)
+}
+
+// constructURLForReportForOrgCluster function constructs an URL to access the
+// endpoint to retrieve results for given cluster from selected organization
+func constructURLForReportForOrgCluster(organizationID string, clusterID string, userID types.UserID) string {
+	url := httputils.MakeURLToEndpoint("", server.ReportEndpoint, organizationID, clusterID, userID)
+	return url[1:]
+}
+
+// constructURLForReportInfoForOrgCluster function constructs an URL to access
+// the endpoint to retrieve results metadata for given cluster from selected
+// organization
+func constructURLForReportInfoForOrgCluster(organizationID string,
+	clusterID string, userID types.UserID) string {
+	url := httputils.MakeURLToEndpoint("", server.ReportMetainfoEndpoint,
+		organizationID, clusterID, userID)
+	return url[1:]
+}
+
+// readStatusFromResponse reads and parses status from response body
+func readStatusFromResponse(f *frisby.Frisby) StatusOnlyResponse {
+	response := StatusOnlyResponse{}
+	text, err := f.Resp.Content()
+	if err != nil {
+		f.AddError(err.Error())
+	} else {
+		err := json.Unmarshal(text, &response)
+		if err != nil {
+			f.AddError(err.Error())
+		}
+	}
+	return response
+}
+
+// statusResponseChecker tests which text is returned in "status" attribute
+func statusResponseChecker(f *frisby.Frisby, expectedStatus string) {
+	response := readStatusFromResponse(f)
+	if response.Status != expectedStatus {
+		f.AddError(fmt.Sprintf("Expected status is '%s', but got '%s' instead", expectedStatus, response.Status))
+	}
+}
+
 // StatusOnlyResponse represents response containing just a status
 type StatusOnlyResponse struct {
 	Status string `json:"status"`
